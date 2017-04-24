dfc13f7647aac82e107d9d122a7955a4e8f83dc4
Erik de Castro Lopo <erik.decastrolopo@ambiata.com>
2017-04-24 16:25:38 +1000

    Add https-server runner script

---
+++ scripts/https-server-runner.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -u
+
+# `killall -HUP https-server-runner.sh` will kill this script.
+
+server=dist/build/https-server/https-server
+
+if test ! -f ${server} ; then
+  echo "Missing '${server}. Need to build it first."
+  exit 1
+  fi
+
+while /bin/true ; do
+  timeout 30 ${server}
+  sleep 2
+  done
